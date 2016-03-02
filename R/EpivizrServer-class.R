2f3b8b29fecd2d95e691ea3457c4428ede744709
Hector Corrada Bravo <hcorrada@gmail.com>
2016-03-02 13:05:22 -0500

    start with server

---
+++ R/EpivizrServer-class.R
@@ -0,0 +1,214 @@
+#' Class providing WebSocket connection server
+#' 
+#' @docType class
+#' @importFrom R6 R6Class
+EpivizServer <- R6Class("EpivizServer",
+  private = list(
+    port = 7312L,
+    try_ports = FALSE,
+    websocket = NULL,
+    server = NULL,
+    interrupted = FALSE,
+    socket_connected = FALSE,
+    verbose = FALSE,
+    request_queue = NULL,
+    request_waiting = FALSE,
+    action_handlers = NULL,
+    callback_array = NULL,
+    daemonized = FALSE,
+    start_server_fn = NULL,
+    stop_server_fn = NULL,
+    try_more_ports = function(callbacks, minPort=7000L, maxPort=7999L) {
+      success <- FALSE
+      private$port <- minPort
+      while(!success && private$port <= maxPort) {
+        tryCatch({
+          cat(".")
+          private$server <- private$start_server_fn("0.0.0.0", private$port, callbacks)
+          success <- TRUE
+        }, error=function(e) {
+          private$port <- private$port + 1L
+        })
+      }
+      invisible()
+  }
+  ),
+  public = list(
+    initialize = function(
+        port=7312L, 
+        try_ports=FALSE, 
+        daemonized=NULL, 
+        verbose=FALSE) {
+      private$port <- port
+      private$try_ports <- try_ports
+      private$daemonized <-  .epivizrCanDaemonize() && isTRUE(daemonized)
+      private$start_server_fn <- if (private$daemonized) httpuv::startDaemonizedServer else httpuv::startServer
+      private$stop_server_fn <- if (.self$daemonized) httpuv::stopDaemonizedServer else httpuv::stopServer
+      private$verbose <- verbose
+      
+      private$request_queue <- Queue$new()
+      private$request_waiting <- FALSE
+      
+      private$action_handlers <- vector("list")
+      
+      private$callback_array <- IndexedArray$new()
+      
+      reg.finalizer(self,
+                    function(e) {
+                      e$stop_server()
+                    }, onexit=TRUE)
+    },
+    show=function() {
+      cat(sprintf("<EpivizServer> port: %d, %s", private$port, ifelse(private$socket_connected,"connected","not connected")),"\n")
+      invisible()
+    },
+    makeCallbacks=function() {
+      wsHandler <- function(ws) {
+        if (verbose) epivizrMsg("WS opened")
+        websocket <<- ws
+        socketConnected <<- TRUE
+        websocket$onMessage(.self$msgCallback)
+        websocket$onClose(function() {
+          socketConnected <<- FALSE
+          invisible()
+        })
+        popRequest()
+        invisible()
+      }
+
+      if (standalone) {
+        httpHandler <- .standalonePage(staticSitePath)
+      } else {
+        httpHandler <- .dummyTestPage
+      }
+
+      handlerMgr <- HandlerManager$new()
+      handlerMgr$addHandler(httpHandler, 'static')
+      handlerMgr$addWSHandler(wsHandler, 'ws')
+      handlerMgr$createHttpuvApp()
+    },
+    startServer=function(...) {
+      'start the websocket server'
+      callbacks <- makeCallbacks()
+      tryCatch({
+        server <<- startServerFn("0.0.0.0", port, callbacks)
+      }, error=function(e) {
+        if (!tryPorts)
+          stop(sprintf("Error starting epivizServer, likely because port %d is in use.\nTry a different port number or setting tryPorts=TRUE (see ?startEpiviz).",port))
+        tryMorePorts(callbacks)
+      })
+      invisible()
+    },
+    stopServer=function() {
+      interrupted <<- TRUE
+
+      if (!isClosed()) {
+        stopServerFn(server)
+      }
+      server <<- NULL
+      socketConnected <<- FALSE
+      interrupted <<- TRUE
+      invisible()
+    },
+    service=function(nonInteractive=FALSE) {
+      if (isClosed()) {
+        stop("Can't listen, socket is closed")
+      }
+
+      if (daemonized)
+        return(invisible(TRUE))
+
+      if (nonInteractive) {
+        # run service loop once
+        httpuv::service()
+        return(invisible(TRUE))
+      }
+
+
+      interrupted <<- FALSE
+      while(!interrupted) {
+        httpuv::service()
+        Sys.sleep(0.001)
+      }
+      invisible(TRUE)
+    },
+    stopService=function() {
+      interrupted <<- TRUE
+      invisible()
+    },
+    runServer=function(...) {
+      startServer(...)
+      on.exit(stopServer())
+      service()
+    },
+    isClosed=function() {
+      is.null(server)
+    },
+    bindManager=function(mgr) {
+      msgCallback <<- function(binary, msg) {
+        if (binary) {
+          msg <- rawToChar(msg)
+        }
+
+        if (verbose) {
+          epivizrMsg("RCVD: ", msg)
+        }
+        msg = fromJSON(msg)
+        if (msg$type == "request") {
+          out=list(type="response",
+            requestId=msg$requestId)
+          msgData=msg$data
+          action=msgData$action
+          # request handling
+# defined here: http://epiviz.github.io/dataprovider-plugins.html
+
+          out$data=NULL
+          if (action == "getAllData") {
+              out$data <- list(msg=msgData$chr)
+          } else {
+              out$data <- mgr$handle(action, msgData)
+          }
+          response=toJSON(out)
+          if (verbose) {
+            epivizrMsg("SEND: ", response)
+          }
+          websocket$send(response)
+        } else if (msg$type == "response") {
+          # TODO: check response success
+          callback = mgr$callbackArray$get(msg$requestId)
+          if (!is.null(callback)) {
+            callback(msg$data)
+          }
+          popRequest()
+        }
+      }
+      invisible()
+    },
+    sendRequest=function(request) {
+      requestQueue$push(request)
+      if (!requestWaiting)
+        popRequest()
+      invisible()
+    },
+    popRequest=function() {
+      if (!socketConnected) {
+        return(invisible())
+      }
+      request <- requestQueue$pop()
+      if (is.null(request)) {
+        requestWaiting <<- FALSE
+        stopService()
+        return(invisible())
+      }
+      request <- toJSON(request)
+      if (verbose) epivizrMsg("SEND: ", request)
+      websocket$send(request)
+      requestWaiting <<- TRUE
+      service()
+    },
+    emptyRequestQueue=function() {
+      requestQueue$empty()
+      inivisible()
+    }
+  )
+)
