f226673aed58e20ab3b5e61a8ad269a7cc44a2e2
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2020-10-08 20:54:27 -0400

    Update zzz.R
    can run in the background on windows

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   tryCatch({
     if (is.function(httpuv::startDaemonizedServer))
-      options(epivizrCanDaemonize = .Platform$OS.type == "unix")
+      options(epivizrCanDaemonize = TRUE)
   }, error=function(e) {
     options(epivizCanDaemonize = FALSE)
   })
